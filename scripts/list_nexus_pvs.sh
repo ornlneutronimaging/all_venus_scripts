cb86cad12e17fb296ac8e829315b2260fe115834
JeanBilheux <bilheuxjm@ornl.gov>
2025-07-30 11:00:11 -0400

    Added some demo of how to use the script. this refs #3

--- scripts/list_nexus_pvs.sh
+++ scripts/list_nexus_pvs.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 pixi run --manifest-path /SNS/VENUS/shared/software/git/all_venus_scripts/pixi.toml list_nexus_pvs "$@"
+
+
+# pixi run --manifest-path /SNS/VENUS/shared/software/git/all_venus_scripts/pixi.toml list_nexus_pvs 9235,9236
+# pixi run --manifest-path /SNS/VENUS/shared/software/git/all_venus_scripts/pixi.toml list_nexus_pvs 9235,9236 --ipts IPTS-35945
