0eed2f71ce6f64ea90443c7b0288b391fb3f404f
Chen Zhang <chenzhang8722@gmail.com>
2025-07-30 09:46:02 -0400

    fix path

--- scripts/list_nexus_pvs.sh
+++ scripts/list_nexus_pvs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pixi run --manifest-path /SNS/VENUS/shared/software/git/all_scripts_git/pixi.toml list_nexus_pvs "$@"
+pixi run --manifest-path /SNS/VENUS/shared/software/git/all_venus_scripts/pixi.toml list_nexus_pvs "$@"
